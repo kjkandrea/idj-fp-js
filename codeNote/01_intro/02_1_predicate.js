10f17d31764442f81c309cf01b50299b7e9bde77
andrea <kjkandrea@gmail.com>
2021-09-01 15:15:14 +0900

    Add predicate practice

---
+++ codeNote/01_intro/02_1_predicate.js
@@ -0,0 +1,5 @@
+function main() {
+  console.log('predicate')
+}
+
+export default main;
--- codeNote/01_intro/index.js
+++ codeNote/01_intro/index.js
@@ -1,16 +1,18 @@
 import addMaker from './01_addMaker.js'
 // import './02_filterOldWay.js'
 import filterFpWay from './02_filterFpWay.js'
+import predicate from './02_1_predicate.js'
 
 const entries = [
   addMaker,
-  filterFpWay
+  filterFpWay,
+  predicate
 ]
 
 const run = chapter => entries[chapter]()
 
 function main() {
-  run(1)
+  run(2)
 }
 
 export default main;
