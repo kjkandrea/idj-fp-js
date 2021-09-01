57cba6ee47683a06427cc2d0fc006ee2c4ea62e9
andrea <kjkandrea@gmail.com>
2021-09-01 13:05:56 +0900

    Add filterFpWay : 1단계 추상화 필기

--- codeNote/01_intro/02_filterFpWay.js
+++ codeNote/01_intro/02_filterFpWay.js
@@ -8,6 +8,16 @@ const users = [
   { id: 7, name: 'HI', age: 24 },
 ]
 
+/**
+ * @filter
+ * filter 함수는 항상 동일하게 동작하는 함수이다.
+
+ * @newList
+ * newList 는 이 함수 내부에서 최초로 만들어졌고 외부의 어떠한 상황이나 상태와도 무관하다.
+ *
+ * @predicate
+ * filter 의 if는 predicate 의 결과에만 의존한다.
+ */
 function filter (list, predicate) {
   const newList = [] // 이전 값(list) 의 상태를 변경하지않고 새로운 리스트를 생성. 불변성과 관련된 중요한 콘셉트.
   for (let i = 0, len = list.length; i < len; i++) {
