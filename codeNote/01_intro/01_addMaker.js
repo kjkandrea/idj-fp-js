8510c88e78a6cf1d8b352a08be2c4f7aa3bc9e58
andrea <kjkandrea@gmail.com>
2021-09-01 12:37:59 +0900

    Use addMaker

--- codeNote/01_intro/01_addMaker.js
+++ codeNote/01_intro/01_addMaker.js
@@ -5,9 +5,12 @@ function addMaker(a) {
 }
 
 function main() {
-  console.log(
-    addMaker(10)(5)
-  )
+  console.log('addMaker(10)(5) :', addMaker(10)(5))
+
+  const add5 = addMaker(5)
+
+  console.log('add5(3)', add5(3))
+  console.log('add5(3)', add5(4))
 }
 
 export default main;
