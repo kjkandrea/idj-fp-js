9cb872443f54273d4e67e8ae8b6179e4510c755c
andrea <kjkandrea@gmail.com>
2021-09-05 15:25:54 +0900

    Add useBind

--- codeNote/02_dojo/03_01_partialApplication.js
+++ codeNote/02_dojo/03_01_partialApplication.js
@@ -1,5 +1,14 @@
+function useBind() {
+  const add = (a, b) => a + b;
+  const add10 = add.bind(null, 10);
+  console.log(
+    add10(20) // 30
+  )
+}
+
 function main() {
   console.log('partialApplication')
+  useBind()
 }
 
 export default main;
