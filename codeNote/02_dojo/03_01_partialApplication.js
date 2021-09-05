189bf4e9f5f39b570a9f4ca4ba72866b3eeb93cf
andrea <kjkandrea@gmail.com>
2021-09-05 15:09:20 +0900

    Add partialApplication file

---
+++ codeNote/02_dojo/03_01_partialApplication.js
@@ -0,0 +1,5 @@
+function main() {
+  console.log('partialApplication')
+}
+
+export default main;
--- codeNote/02_dojo/index.js
+++ codeNote/02_dojo/index.js
@@ -2,18 +2,20 @@ import firstClassFunction from './01_fisrtClassFunction.js'
 import closure from './02_00_closure.js'
 import usefulClosure from './02_01_usefulClosure.js'
 import highOrderFunction from './03_highOrderFunction.js'
+import partialApplication from './03_01_partialApplication.js'
 
 const entries = [
   firstClassFunction,
   closure,
   usefulClosure,
-  highOrderFunction
+  highOrderFunction,
+  partialApplication
 ]
 
 const run = chapter => entries[chapter]()
 
 function main () {
-  run(3)
+  run(4)
 }
 
 export default main
