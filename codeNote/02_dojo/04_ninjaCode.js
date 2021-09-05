9bcf684bcccfc30d4faafa73f4f4f12cdebee693
andrea <kjkandrea@gmail.com>
2021-09-05 16:49:35 +0900

    Add ninjaCode

--- codeNote/02_dojo/04_ninjaCode.js
+++ codeNote/02_dojo/04_ninjaCode.js
@@ -1,5 +1,42 @@
+function objectKeyNinja() {
+  const obj = {
+    [(() => 'a')()]: 1
+  }
+
+  obj[(() => 'b')()] = 2;
+
+  console.log(obj)
+}
+
+function hoistingNinja() {
+  console.log(one)
+  console.log(fOne)
+
+  try {
+    console.log(fOne);
+  } catch (err) { // no catch
+    console.log(err)
+  }
+
+  var one = 1;
+  var fOne = () => '1'
+
+  function add(a, b) {
+    return valid() ? a + b : '🙃';
+
+    function valid() {
+      return Number.isInteger(a) && Number.isInteger(b);
+    }
+  }
+
+  console.log(add(10, 5))
+  console.log(add('🐕', 1004)) // 🙃
+
+}
+
 function main() {
-  console.log('👽')
+  objectKeyNinja()
+  hoistingNinja()
 }
 
 export default main
