a22c6d049db0af3d94abc89bb726590d1b6c31d7
andrea <kjkandrea@gmail.com>
2021-09-05 15:40:01 +0900

    Add johnResigPartial

--- codeNote/02_dojo/03_01_partialApplication.js
+++ codeNote/02_dojo/03_01_partialApplication.js
@@ -6,9 +6,37 @@ function useBind() {
   )
 }
 
+// John Resig partial
+
+function johnResigPartial() {
+  Function.prototype.partial = function() {
+    const fn = this
+    const args = Array.prototype.slice.call(arguments) // 1. 인자를 배열로 담아둔다.
+
+    console.log(fn, args) // [Function: abc] [ undefined, 'b', undefined ]
+
+    return function() {
+      let arg = 0;
+      for (let i = 0, len = args.length; arg < len;i++) {
+        if (args[i] === undefined) args[i] = arguments[arg++]
+      }
+      return fn.apply(this, args)
+    }
+  }
+
+  function abc(a, b, c) {
+    console.log(a, b, c)
+  }
+
+  const ac = abc.partial(undefined, 'b', undefined)
+  ac()
+  ac('a', 'c')
+}
+
 function main() {
   console.log('partialApplication')
   useBind()
+  johnResigPartial()
 }
 
 export default main;
