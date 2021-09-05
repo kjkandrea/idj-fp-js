8180d73f61a8cf3f4398dfdaf70f977a4658f6d8
andrea <kjkandrea@gmail.com>
2021-09-05 17:32:37 +0900

    Add namedFunction

--- codeNote/02_dojo/04_ninjaCode.js
+++ codeNote/02_dojo/04_ninjaCode.js
@@ -33,7 +33,7 @@ function hoistingNinja() {
   console.log(add('🐕', 1004)) // 🙃
 }
 
-function IIFE_Ninja() {
+function IIFE_ninja() {
   !function(a) {
     console.log(a)
   }(1)
@@ -49,10 +49,29 @@ function IIFE_Ninja() {
   }
 }
 
+function IIFE_ninjaExpert() {
+  const a = function(a) {
+    console.log(this, a) // [1] 1
+  }.call([1], 1)
+}
+
+function namedFunction() {
+  function flatten(arr) {
+    return function f(arr, new_arr) { // namedFunction 을 이용하여 재귀 구현
+      arr.forEach(v => Array.isArray(v)? f(v, new_arr) : new_arr.push(v));
+      return new_arr;
+    }(arr, [])
+  }
+
+  console.log(flatten([1, [2, [3, 4]]]))
+}
+
 function main() {
   objectKeyNinja()
   hoistingNinja()
-  IIFE_Ninja()
+  IIFE_ninja()
+  IIFE_ninjaExpert()
+  namedFunction()
 }
 
 export default main
