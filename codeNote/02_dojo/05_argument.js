bd877b0257ad3f585f16241888d2cc7d9562fee1
andrea <kjkandrea@gmail.com>
2021-09-05 18:12:12 +0900

    Add arguments diff reason

--- codeNote/02_dojo/05_argument.js
+++ codeNote/02_dojo/05_argument.js
@@ -6,6 +6,11 @@ function argumentLog() {
 
   test(1)
   test(1, 2) // 브라우저에서는 1, 10 나오는데 node 에서는 1, 2 나오네 허허
+  /**
+   * https://stackoverflow.com/questions/15705756/function-arguments-object-in-node-js-is-different-to-chromes-javascript/15705938
+   *
+   * chrome 과 node 가 argument 를 취급하는 방법이 다른듯? 지금 너무 이해하려고 하진 말자.
+   */
 }
 
 function main() {
