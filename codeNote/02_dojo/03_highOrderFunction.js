6f7140b27702392a6e96359d3a2ff1ea01e90530
andrea <kjkandrea@gmail.com>
2021-09-05 14:29:33 +0900

    Add hof basic

--- codeNote/02_dojo/03_highOrderFunction.js
+++ codeNote/02_dojo/03_highOrderFunction.js
@@ -1,8 +1,28 @@
 /*
  */
 
+function callWith10(val, func) {
+  return func(10, val)
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+
+
 function main() {
-  console.log('high order function')
+  console.log(
+    'high order function',
+    '\ncallWith10(20, add)',
+    callWith10(20, add),
+    '\ncallWith10(20, sub)',
+    callWith10(20, sub),
+  )
 }
 
 export default main;
