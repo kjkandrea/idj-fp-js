3678a002c0d458c35b74cca2fb90854abed11ebe
andrea <kjkandrea@gmail.com>
2021-09-11 16:59:55 +0900

    Add get length

--- codeNote/03_underscore/assistant.js
+++ codeNote/03_underscore/assistant.js
@@ -1,3 +1,9 @@
+export const getLength = list => list == null ? void 0 : list.length;
+
 export const assistant = () => console.log(
-  'assistant'
+  getLength([1,2,3]),
+  getLength(1),
+  getLength(null),
+  getLength(undefined),
+  getLength({ length: 3 })
 )
