c47a1737ca17b1991316ece52632c52224286578
andrea <kjkandrea@gmail.com>
2021-09-11 16:53:15 +0900

    Add my map

--- codeNote/03_underscore/underscore.js
+++ codeNote/03_underscore/underscore.js
@@ -1,4 +1,14 @@
+const _ = {}
+
+_.map = (list, iteratee) => {
+  const newList = [];
+  for (let i = 0, len = list.length; i < len; i++)
+    newList.push(iteratee(list[i]))
+  return newList;
+}
+
+
 export const test = () =>
   console.log(
-    'halo'
+    _.map([1,2,3], num => num * 2)
   )
