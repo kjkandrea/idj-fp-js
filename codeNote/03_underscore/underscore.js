e8424c865827dcf6839693950b376879c7fe14aa
andrea <kjkandrea@gmail.com>
2021-09-11 17:30:28 +0900

    Add keys
    재밋당

--- codeNote/03_underscore/underscore.js
+++ codeNote/03_underscore/underscore.js
@@ -11,28 +11,41 @@ const _ = {}
 // }
 
 _.map = (data, iteratee) => {
-  const newList = [];
+  const newList = []
   if (isArrayLike(data)) {
-    for (let i = 0, len = data.length; i < len ; i ++ )
+    for (let i = 0, len = data.length; i < len; i++) {
       newList.push(iteratee(data[i], i, data))
-  } else {
-    for (let key in data)
-      if (data.hasOwnProperty(key)) newList.push(iteratee(data[key], key, data))
+    }
   }
-  return newList;
+  else {
+    for (let key in data) {
+      if (data.hasOwnProperty(key)) {
+        newList.push(iteratee(data[key], key, data))
+      }
+    }
+  }
+  return newList
 }
 
 _.identity = v => v
 
 _.values = list => _.map(list, _.identity)
 
+_.args0 = _.identity
+_.args1 = (a, b) => b
+
+_.keys = list => _.map(list, _.args1)
+
 export const test = () =>
   console.log(
     'map :',
-    _.map([1,2,3], num => num * 2), //array
+    _.map([1, 2, 3], num => num * 2), //array
     _.map({ a: 3, b: 2, c: 1 }, num => num * 2), //array like
-    _.map([1,2,3], function(num){ return num * this }.bind(5)),
+    _.map([1, 2, 3], function (num) { return num * this }.bind(5)),
     '\nvalues :',
-    _.values([1,2,3]),
-    _.values({ a: 3, b: 2, c: 1})
+    _.values([1, 2, 3]),
+    _.values({ a: 3, b: 2, c: 1 }),
+    '\nkeys :',
+    _.keys([1,2,3]),
+    _.keys({ a: 3, b: 2, c: 1 })
   )
