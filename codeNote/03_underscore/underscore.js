ee15ffda11f89a1bd3f3f55479d19dd40ce62ab1
andrea <kjkandrea@gmail.com>
2021-09-11 17:13:59 +0900

    Add sexy map

--- codeNote/03_underscore/underscore.js
+++ codeNote/03_underscore/underscore.js
@@ -1,9 +1,24 @@
+import { isArrayLike } from './assistant.js'
+
 const _ = {}
 
-_.map = (list, iteratee) => {
+// old version
+// _.map = (list, iteratee) => {
+//   const newList = [];
+//   for (let i = 0, len = list.length; i < len; i++)
+//     newList.push(iteratee(list[i], i, list))
+//   return newList;
+// }
+
+_.map = (data, iteratee) => {
   const newList = [];
-  for (let i = 0, len = list.length; i < len; i++)
-    newList.push(iteratee(list[i], i, list))
+  if (isArrayLike(data)) {
+    for (let i = 0, len = data.length; i < len ; i ++ )
+      newList.push(iteratee(data[i], i, data))
+  } else {
+    for (let key in data)
+      if (data.hasOwnProperty(key)) newList.push(iteratee(data[i], i, data))
+  }
   return newList;
 }
 
