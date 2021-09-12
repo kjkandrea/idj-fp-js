8506bdffe6d2f4dfb9bcae6a31ef53cd96a9dacf
andrea <kjkandrea@gmail.com>
2021-09-12 19:09:48 +0900

    Add model items

--- codeNote/model/users.js
+++ codeNote/model/users.js
@@ -3,6 +3,8 @@ const users = {
     { id: 1, name: 'JK', age: 30 },
     { id: 2, name: 'SJ', age: 31 },
     { id: 3, name: 'MM', age: 2 },
+    { id: 4, name: 'HY', age: 28 },
+    { id: 5, name: 'MV', age: 31 },
   ],
   get() {
    return users.data;
