af1e19ee815ded6c8c7d2606b90f9f06c8ee2c72
andrea <kjkandrea@gmail.com>
2021-09-12 16:38:43 +0900

    Add splice chapter
    터미널이 넘 지저분

--- codeNote/03_underscore/underscore.js
+++ codeNote/03_underscore/underscore.js
@@ -47,68 +47,77 @@ _.filter = bloop(_.array,
   (boolean, object, value) => boolean && _.pushTo(value, object))
 
 export const test = () => {
-  console.log(
-    'map :',
-    _.map([1, 2, 3], num => num * 2), //array
-    _.map({ a: 3, b: 2, c: 1 }, num => num * 2), //array like
-    _.map([1, 2, 3], function (num) { return num * this }.bind(5)),
-    '\nvalues :',
-    _.values([1, 2, 3]),
-    _.values({ a: 3, b: 2, c: 1 }),
-    '\nkeys :',
-    _.keys([1, 2, 3]),
-    _.keys({ a: 3, b: 2, c: 1 }),
-    '\neach :',
-  )
-  _.each([1, 2, 3], console.log)
-  _.each({ a: 3, b: 2, c: 1 }, console.log)
-  console.log(
-    '\n_.keys vs Object.keys',
-    _.keys({ puppy: '🐶' }), // puppy
-    Object.keys({ puppy: '🐶' }), // puppy
-    _.keys(10), // []
-    Object.keys(10), // []
-    _.keys(null), // []
-    // Object.keys(null) // TypeError: Cannot convert undefined or null to
-    // object
-  )
-  console.log(
-    '\nfilter :',
-    _.filter([1, 2, 3], v => v > 2),
-    _.filter({ a: 1, b: 2, c: 3 }, v => v > 2),
-  )
-  const sum = (a, b, c, d) => (a || 0) + (b || 0) + (c || 0) + (d || 0)
-  console.log(
-    '\nrester',
-    _.rester(sum)(1, 2, 3, 4),
-    _.rester(sum, 2)(1, 2, 3, 4),
-    _.rester(sum, 3)(1, 2, 3, 4),
-  )
+  const chapter1 = () => {
+    console.log(
+      'map :',
+      _.map([1, 2, 3], num => num * 2), //array
+      _.map({ a: 3, b: 2, c: 1 }, num => num * 2), //array like
+      _.map([1, 2, 3], function (num) { return num * this }.bind(5)),
+      '\nvalues :',
+      _.values([1, 2, 3]),
+      _.values({ a: 3, b: 2, c: 1 }),
+      '\nkeys :',
+      _.keys([1, 2, 3]),
+      _.keys({ a: 3, b: 2, c: 1 }),
+      '\neach :',
+    )
+    _.each([1, 2, 3], console.log)
+    _.each({ a: 3, b: 2, c: 1 }, console.log)
+    console.log(
+      '\n_.keys vs Object.keys',
+      _.keys({ puppy: '🐶' }), // puppy
+      Object.keys({ puppy: '🐶' }), // puppy
+      _.keys(10), // []
+      Object.keys(10), // []
+      _.keys(null), // []
+      // Object.keys(null) // TypeError: Cannot convert undefined or null to
+      // object
+    )
+  }
 
-  _.rester(console.log, 2)(1, 2, 3, 4)
+  const chapter2 = () => {
+    console.log(
+      '\nfilter :',
+      _.filter([1, 2, 3], v => v > 2),
+      _.filter({ a: 1, b: 2, c: 3 }, v => v > 2),
+    )
+    const sum = (a, b, c, d) => (a || 0) + (b || 0) + (c || 0) + (d || 0)
+    console.log(
+      '\nrester',
+      _.rester(sum)(1, 2, 3, 4),
+      _.rester(sum, 2)(1, 2, 3, 4),
+      _.rester(sum, 3)(1, 2, 3, 4),
+    )
 
-  const sub = (a, b) => a - b;
-  const sub2 = _.if(
-    (a, b) => a >= b,
-    sub,
-    () => new Error('a가 b보다 작습니다.')
-  )
-  console.log(
-    '\nsub2',
-    sub2(10, 5),
-    // sub2(5, 10), // Error: a가 b보다 작습니다.
-  )
+    _.rester(console.log, 2)(1, 2, 3, 4)
 
-  const diff = _.if(
-    (a, b) => a >= b,
-    sub,
-    (a, b) => sub(b, a)
-  )
-  console.log(
-    '\ndiff',
-    diff(9, 1),
-    diff(5, 8)
-  )
+    const sub = (a, b) => a - b;
+    const sub2 = _.if(
+      (a, b) => a >= b,
+      sub,
+      () => new Error('a가 b보다 작습니다.')
+    )
+    console.log(
+      '\nsub2',
+      sub2(10, 5),
+      // sub2(5, 10), // Error: a가 b보다 작습니다.
+    )
+
+    const diff = _.if(
+      (a, b) => a >= b,
+      sub,
+      (a, b) => sub(b, a)
+    )
+    console.log(
+      '\ndiff',
+      diff(9, 1),
+      diff(5, 8)
+    )
+  }
+
+  // runner
+  // chapter1()
+  chapter2()
 }
 
 export default _
