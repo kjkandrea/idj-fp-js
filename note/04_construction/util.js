85dba939457b532ce01c23a741f79042c5866b4a
andrea <kjkandrea@gmail.com>
2021-09-19 15:30:45 +0900

    Add idMaker

--- note/04_construction/util.js
+++ note/04_construction/util.js
@@ -1,32 +1,47 @@
 const once = func => {
-  let flag, result;
+  let flag, result
   return function () {
-    if (flag) return result;
-    flag = true;
+    if (flag) {
+      return result
+    }
+    flag = true
     return result = func.apply(this, arguments)
   }
 }
 
 const skip = body => { // confirm 과 함께 쓸듯
   let yes
-  return function() {
+  return function () {
     return yes || (yes = body.apply(null, arguments))
   }
 }
 
-function main() {
-  const printOnce = once(()=> console.log('once'))
+const idMaker = startNumber => () => startNumber++
+
+function main () {
+  const printOnce = once(() => console.log('once'))
   printOnce()
   printOnce()
   printOnce()
   printOnce()
 
+  const printSkip = skip(() => {
+    console.log('skip')
+    return true
+  })
+  printSkip()
+  printSkip()
+  printSkip()
+  printSkip()
 
-  const printSkip = skip(() => { console.log('skip'); return true })
-  printSkip()
-  printSkip()
-  printSkip()
-  printSkip()
+  console.log('idMaker')
+  const getCartId = idMaker(10)
+  console.log(
+    getCartId(),
+    getCartId(),
+    getCartId(),
+    getCartId(),
+  )
 }
 
 export default main;
