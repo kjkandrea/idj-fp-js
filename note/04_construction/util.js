856f8317b027157894dcd2569dde6a97ae131c0b
andrea <kjkandrea@gmail.com>
2021-09-19 15:19:07 +0900

    Add skip

--- note/04_construction/util.js
+++ note/04_construction/util.js
@@ -7,12 +7,26 @@ const once = func => {
   }
 }
 
+const skip = body => {
+  let yes
+  return function() {
+    return yes || (yes = body.apply(null, arguments))
+  }
+}
+
 function main() {
   const now = once(()=> Date.now())
   const twoSecondAfterNow = () => setTimeout(() => console.log(now()), 2000)
 
   console.log(now(), now())
   twoSecondAfterNow()
+
+
+  const printOnce = skip(() => { console.log('skip'); return true })
+  printOnce()
+  printOnce()
+  printOnce()
+  printOnce()
 }
 
 export default main;
