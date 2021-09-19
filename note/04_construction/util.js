81af599aa942b34f26c9a174d8213f80d0676c03
andrea <kjkandrea@gmail.com>
2021-09-19 15:15:00 +0900

    Add once

--- note/04_construction/util.js
+++ note/04_construction/util.js
@@ -1,5 +1,18 @@
+const once = func => {
+  let flag, result;
+  return function () {
+    if (flag) return result;
+    flag = true;
+    return result = func.apply(this, arguments)
+  }
+}
+
 function main() {
-  console.log('run')
+  const now = once(()=> Date.now())
+  const twoSecondAfterNow = () => setTimeout(() => console.log(now()), 2000)
+
+  console.log(now(), now())
+  twoSecondAfterNow()
 }
 
 export default main;
