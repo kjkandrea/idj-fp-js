515930e6c88fb77f0a65eba2a18ffd5f40da4e1c
andrea <kjkandrea@gmail.com>
2021-09-20 15:56:41 +0900

    View Lazy inside

--- note/06_lazy/init.js
+++ note/06_lazy/init.js
@@ -13,5 +13,8 @@ _.go(
   [2, 4, 11, 2, 7, 12],
   L.map(square), // 3번 반복
   L.every(v => v < 100), // 3번 반복
+
+  // 내부적으로 동작하는 모습은 다음과 같다.
+  // _.every(_.pipe(square, v => v < 100))
   console.log, // false
 )
