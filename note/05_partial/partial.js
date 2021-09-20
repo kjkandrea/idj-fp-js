3012020e21ab6431a2cb4c18b66c2714d11192f1
andrea <kjkandrea@gmail.com>
2021-09-20 14:20:41 +0900

    Use partial

--- note/05_partial/partial.js
+++ note/05_partial/partial.js
@@ -7,3 +7,11 @@ const concat = _.partial('concat')
 console.log(
   concat([1, 2], 3)
 )
+
+// 부분 커링
+
+const values = data => _.map(data, v => v)
+
+console.log(
+  values ({ a: 1, b: 2, c: 4 })
+)
