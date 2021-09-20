315a73508dae6a3425c8a267fe17f9e2764b47ab
andrea <kjkandrea@gmail.com>
2021-09-20 13:38:01 +0900

    Add joinUser

---
+++ note/practice/auth_pipeline.js
@@ -0,0 +1,30 @@
+import _ from 'underscore'
+
+/* pipe */
+const pipeline = function() {
+  const functions = arguments;
+  return seed => _.reduce(functions, (l, r) => r(l), seed)
+}
+
+const users = []
+const companies = []
+
+const joinAt = attrs =>
+  _.extend(attrs, { joinAt: new Date() })
+
+const greeting = member =>
+  `${member.name} 회원님은 ${member.id}번째 회원이십니다. 환영합니다.`
+
+const joinUser = pipeline(
+  joinAt,
+  member => {
+    users.push(member)
+    member.id = users.length;
+    return member
+  },
+  greeting
+)
+
+console.log(
+  joinUser({ name: 'JK' })
+)
